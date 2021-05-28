365185c8dec023151590fc877a7c32c0b2bba95e
Orphée <letembeorphee@gmail.com>
2021-05-28 20:45:01 +0200

    Ajout de fonction et pandas

--- P2_01_codesource.py
+++ P2_01_codesource.py
@@ -8,50 +8,101 @@ Programme d'extraction des informations produits de la catégorie travel du site
 import requests
 from bs4 import BeautifulSoup 
 import csv
+import pandas
 
-# Ouverture du fichier csv
-csv_file = open("category.csv", "w",encoding="utf-8")
-csv_writer = csv.writer(csv_file,delimiter=",",lineterminator="\n")
 
-#Définition des entêtes de colonnes
-csv_writer.writerow(["Product_page_url" , "Universal_Product_Code" , "Title" , "Price_including_tax" , "Price_excluding_tax" , "Number_avaible" , "Product_description" , "Category" , "Review_rating" , "Image_url"])
 
-# Etractiontion de la page caterogie
-url_category = "https://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+url_category = "https://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
 
-reponse = requests.get(url_category)
 
-if reponse.ok:
-	liens = []
-	soup = BeautifulSoup(reponse.text,"html.parser")
-	places = soup.findAll("h3")
-	for place in places:
-		a = place.find("a")["href"].split("..")[3]
-		lien = "https://books.toscrape.com/catalogue" + a
 
-		r = requests.get(lien)
-
-		if r.ok:
-			soup = BeautifulSoup(r.text,"html.parser")
-			product_page_url = lien
-			universal_product_code = soup.find("table",{"class":"table table-striped"}).findAll("td")[0].text
-			title = soup.find("div", {"class":"col-sm-6 product_main"}).h1.text
-			price_including_tax = soup.find("table",{"class":"table table-striped"}).findAll("td")[2].text
-			price_excluding_tax = soup.find("table",{"class":"table table-striped"}).findAll("td")[3].text
-			number_avaible = soup.find("table",{"class":"table table-striped"}).findAll("td")[5].text
-			product_description = soup.findAll("p")[3].text
-			category = soup.find("ul",{"class":"breadcrumb"}).findAll("li")[2].text
-			review_rating = soup.find("table",{"class":"table table-striped"}).findAll("td")[6].text
-			image_src = soup.find("div",{"class":"item active"}).find("img")["src"].split("..")[2]
-			image_url = "https://books.toscrape.com" + image_src
-
-			#Ecriture des informations dans le fichier csv
-			csv_writer.writerow([product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax, number_avaible, product_description, category, review_rating, image_url ])
-					
-		#fermeture du fichier csv
-csv_file.close()
-		
+ #Création du dictionnaire 
+data = {
 	
+		"Product_page_url":[],
+		"Universal_Product_Code":[],
+		"Title":[],
+		"Price_including_tax":[],
+		"Price_excluding_tax":[],
+		"Number_avaible":[],
+		"Product_description":[],
+		"Category":[],
+		"Review_rating":[],
+		"Image_url":[]
+}
+
+# fonction de création du csv
+def exportation_info(data):
+	df = pandas.DataFrame(data, columns= ["Product_page_url" , "Universal_Product_Code" , "Title" , 
+	"Price_including_tax" , "Price_excluding_tax" , "Number_avaible" , 
+	"Product_description" , "Category" , "Review_rating" , "Image_url"
+	])
+
+	df.to_csv("InfoTarifBTS.csv", index=False)
+
+# Fonction d'extraction des informations produits
+def extraction_info_product(url):
+	r = requests.get(url)
+
+	if r.ok:
+		soup = BeautifulSoup(r.text,"html.parser")
+		product_page_url = link_product
+		universal_product_code = soup.find("table",{"class":"table table-striped"}).findAll("td")[0].text
+		title = soup.find("div", {"class":"col-sm-6 product_main"}).h1.text
+		price_including_tax = soup.find("table",{"class":"table table-striped"}).findAll("td")[2].text
+		price_excluding_tax = soup.find("table",{"class":"table table-striped"}).findAll("td")[3].text
+		number_avaible = soup.find("table",{"class":"table table-striped"}).findAll("td")[5].text
+		product_description = soup.findAll("p")[3].text
+		category = soup.find("ul",{"class":"breadcrumb"}).findAll("li")[2].text
+		review_rating = soup.find("table",{"class":"table table-striped"}).findAll("td")[6].text
+		image_src = soup.find("div",{"class":"item active"}).find("img")["src"].split("..")[2]
+		image_url = "https://books.toscrape.com" + image_src
+
+
+		data["Product_page_url"].append(product_page_url)
+		data["Universal_Product_Code"].append(universal_product_code)
+		data["Title"].append(title)
+		data["Price_including_tax"].append(price_including_tax)
+		data["Price_excluding_tax"].append(price_excluding_tax)
+		data["Number_avaible"].append(number_avaible)
+		data["Product_description"].append(product_description)
+		data["Category"].append(category)
+		data["Review_rating"].append(review_rating)
+		data["Image_url"].append(image_url)
+
+
+
+#fonction d'extraction des liens
+def extraction_link(url):
+	global link_product
+	global next_page_url
+	global next_page_text
+
+	reponse = requests.get(url)
+
+	if reponse.ok:
+		soup = BeautifulSoup(reponse.text,"html.parser")
+			#next_page_text = soup.find("li", {"class":"next"}).find("a").text
+			#next_page_href = soup.find("li", {"class":"next"}).find("a")["href"]
+			#next_page_url = url_category.split("index")[0] + next_page_href
+		links = soup.findAll("h3")
+
+		for link in links:
+			a = link.find("a")["href"].split("..")[3]
+			link_product = "https://books.toscrape.com/catalogue" + a
+			extraction_info_product(link_product)
+
+	
+
+
+
+extraction_link(url_category)	
+exportation_info(data)
+
+
+
+
+			
 
 
 
