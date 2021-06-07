9e08de6c9fea3211a4e8a40ee1a2d4c863736660
Orphée <letembeorphee@gmail.com>
2021-06-07 10:17:36 +0200

    Ajout de la fonction dédiée à l'analyse des url

--- P2_01_codesource.py
+++ P2_01_codesource.py
@@ -1,150 +1,149 @@
 
 # -*- coding: utf-8 -*-
-"""
-Programme d'extraction des informations produits du site books to scrape
-"""
 
-import requests
-import pandas
+"""
+The purpose of this program is to extract information from
+the products on the Books to Scrap site.
+
+"""
+# Import needed package
 import os
-from bs4 import BeautifulSoup 
+import requests
+
+import pandas
+from bs4 import BeautifulSoup
 
 
+url_site = 'https://books.toscrape.com/index.html'
 
-url_page = 'https://books.toscrape.com/index.html' 
-
-# Création de la liste des liens des différentes catégories
 category_link_list = []
 
-#Création du dictionnaire de stockage des infos produit
-data = {
-		'Product_page_url':[],
-		'Universal_Product_Code':[],
-		'Title':[],
-		'Price_including_tax':[],
-		'Price_excluding_tax':[],
-		'Number_avaible':[],
-		'Product_description':[],
-		'Category':[],
-		'Review_rating':[],
-		'Image_url':[]
-		
-		}
-
-# fonction de création des dossiers par catégorie 
-def export_data(info):
-	file_name = category.split('\n')[1]
-	os.mkdir(file_name)
-
-	df = pandas.DataFrame(data, columns= ['Product_page_url' , 'Universal_Product_Code' , 'Title' , 
-	'Price_including_tax' , 'Price_excluding_tax' , 'Number_avaible' , 
-	'Product_description' , 'Category' , 'Review_rating' , 'Image_url'
-	])
-	
-	df.to_csv(file_name + '\\' + 'books.csv', index=False) 
-
-	for i in range(len(data['Image_url'])):
-		r = requests.get(data['Image_url'][i])
-		with open(file_name + '\\' + 'image' + str(i+1) + '.jpg', 'wb') as f: 
-			f.write(r.content)
-
-	data['Product_page_url'] =[]
-	data['Universal_Product_Code'] =[]
-	data['Title'] =[]
-	data['Price_including_tax'] =[]
-	data['Price_excluding_tax'] =[]
-	data['Number_avaible'] =[]
-	data['Product_description'] =[]
-	data['Category'] =[]
-	data['Review_rating'] =[]
-	data['Image_url'] =[]
+# Dictionary definition
+data = {'Product_page_url': [],
+        'Universal_Product_Code': [],
+        'Title': [],
+        'Price_including_tax': [],
+        'Price_excluding_tax': [],
+        'Number_avaible': [],
+        'Product_description': [],
+        'Category': [],
+        'Review_rating': [],
+        'Image_url': []}
 
 
-#fonction d'extraction des informations produits
+def parse_url(url):
+    """ This function parses the url passed as argument
+    and returns the html page in text format.
+    """
+
+    response = requests.get(url)
+    if response.ok:
+        return BeautifulSoup(response.text, 'html.parser')
+
+
 def extract_product_info(url):
+    """This function extracts the information from the products
+    and loads them into the dictionary.
+    """
 
-	global category
-	
-	reponse = requests.get(url)
+    soup = parse_url(url)
+    for link in soup.findAll('h3'):
+        product_partial_link = link.find('a')['href'].split('..')[3]
+        link_product = 'https://books.toscrape.com/catalogue'\
+                       + product_partial_link
+        soup = parse_url(link_product)
+        product_page_url = link_product
+        universal_product_code = soup.findAll('td')[0].text
+        title = soup.find('h1').text
+        price_incl_tax = soup.findAll('td')[2].text
+        price_excl_tax = soup.findAll('td')[3].text
+        number_avaible = soup.findAll('td')[5].text
+        product_description = soup.findAll('p')[3].text
+        category = soup.findAll('li')[2].text
+        review_rating = soup.findAll('td')[6].text
+        image_src = soup.find('img')['src'].split('..')[2]
+        image_url = 'https://books.toscrape.com' + image_src
 
-	if reponse.ok:
-		soup = BeautifulSoup(reponse.text,'html.parser')
-		links = soup.findAll('h3')
-
-		for link in links:
-			a = link.find('a')['href'].split('..')[3]
-			link_product = 'https://books.toscrape.com/catalogue' + a
-			r = requests.get(link_product)
-
-			if r.ok:
-				soup = BeautifulSoup(r.text,'html.parser')
-				product_page_url = link_product
-				universal_product_code = soup.find('table',{'class':'table table-striped'}).findAll('td')[0].text
-				title = soup.find('div', {'class':'col-sm-6 product_main'}).h1.text
-				price_including_tax = soup.find('table',{'class':'table table-striped'}).findAll('td')[2].text
-				price_excluding_tax = soup.find('table',{'class':'table table-striped'}).findAll('td')[3].text
-				number_avaible = soup.find('table',{'class':'table table-striped'}).findAll('td')[5].text
-				product_description = soup.findAll('p')[3].text
-				category = soup.find('ul',{'class':'breadcrumb'}).findAll('li')[2].text
-				review_rating = soup.find('table',{'class':'table table-striped'}).findAll('td')[6].text
-				image_src = soup.find('div',{'class':'item active'}).find('img')['src'].split('..')[2]
-				image_url = 'https://books.toscrape.com' + image_src
-				
-				data['Product_page_url'].append(product_page_url)
-				data['Universal_Product_Code'].append(universal_product_code)
-				data['Title'].append(title)
-				data['Price_including_tax'].append(price_including_tax)
-				data['Price_excluding_tax'].append(price_excluding_tax)
-				data['Number_avaible'].append(number_avaible)
-				data['Product_description'].append(product_description)
-				data['Category'].append(category)
-				data['Review_rating'].append(review_rating)
-				data['Image_url'].append(image_url)
+        data['Product_page_url'].append(product_page_url)
+        data['Universal_Product_Code'].append(universal_product_code)
+        data['Title'].append(title)
+        data['Price_including_tax'].append(price_incl_tax)
+        data['Price_excluding_tax'].append(price_excl_tax)
+        data['Number_avaible'].append(number_avaible)
+        data['Product_description'].append(product_description)
+        data['Category'].append(category)
+        data['Review_rating'].append(review_rating)
+        data['Image_url'].append(image_url)
 
 
-#foncion de selection des pages des catégories
-def parse_category_page(url):
-	r = requests.get(url)
-	soup = BeautifulSoup(r.text, 'html.parser')
-	a = soup.find('form', {'class':'form-horizontal'}).findAll('strong')
+def export_data(info):
+    """This function creates a folder to store the collected information
+    in csv format as well as the images.
+    """
+  
+    folder_name = data['Category'][0].split('\n')[1]
+    os.mkdir(folder_name)
 
-	if len(a) == 1:
-		extract_product_info(url)
-		export_data(data)
-	else:
-		next_page_text = soup.find('li', {'class':'current'}).text.strip()
-		next_page_number = next_page_text.split(' ')[3]
-		#next_page_href = soup.find('li', {'class':'next'}).find('a')['href']
+    df = pandas.DataFrame(data, columns=[
+        'Product_page_url', 'Universal_Product_Code',
+        'Title', 'Price_including_tax', 'Price_excluding_tax',
+        'Number_avaible', 'Product_description', 'Category',
+        'Review_rating', 'Image_url'
+        ])
 
-		for i in range (1, int(next_page_number)+1):
-			next_page_url = url.split('index')[0] + 'page-' + str(i) + '.html'
-			r = requests.get(next_page_url)
-			soup = BeautifulSoup(r.text, 'html.parser')
-			next_page_text = soup.find('li', {'class':'current'}).text.strip()
-			page_num = next_page_text.split(' ')[1]
-			
-			if int(page_num) < int(next_page_number) : 
-				extract_product_info(next_page_url)				
-			else:
-				extract_product_info(next_page_url)
-				export_data(data)
-				
-																
-#fonction d'extration des pages de catégorie
-def parse_url_page(url):
-	r = requests.get(url)
-	if r.ok:
-		soup = BeautifulSoup(r.text,'html.parser')
-		for i in range (1, 51):
-			url_category = soup.find('ul', {'class':'nav nav-list'}).findAll('a')[i]['href']
-			category_link = url_page.split('index')[0]+ url_category
-			category_link_list.append(category_link)
+    df.to_csv(folder_name + '\\' + 'books.csv', index=False)
 
-	for url in category_link_list:
-		parse_category_page(url)
+    for i in range(len(data['Image_url'])):
+        r = requests.get(data['Image_url'][i])
+        with open(folder_name + '\\' + 'image' + str(i+1) + '.jpg', 'wb') as f:
+            f.write(r.content)
+
+    data['Product_page_url'] = []
+    data['Universal_Product_Code'] = []
+    data['Title'] = []
+    data['Price_including_tax'] = []
+    data['Price_excluding_tax'] = []
+    data['Number_avaible'] = []
+    data['Product_description'] = []
+    data['Category'] = []
+    data['Review_rating'] = []
+    data['Image_url'] = []
+
+      
 
 
-parse_url_page(url_page)
+def start(url):
+    """This function extracts the url of the different categories
+    and product pages"""
 
-	
+    soup = parse_url(url_site)
+    for i in range(1, 51):
+        category_partial_link = soup.find('ul', {'class': 'nav nav-list'}).findAll('a')[i]['href']
+        category_link = url_site.split('index')[0] + category_partial_link
+        category_link_list.append(category_link)
 
+    for url_category in category_link_list:
+        soup = parse_url(url_category)
+        size_book_display = soup.find('form', {'class': 'form-horizontal'}).findAll('strong')
+
+        if len(size_book_display) == 1:
+            extract_product_info(url_category)
+            export_data(data)
+        else:
+            page_number_text = soup.find('li', {'class': 'current'}).text.strip()
+            number_of_page = page_number_text.split(' ')[3]
+
+            for i in range(1, int(number_of_page)+1):
+                next_page_url = url_category.split('index')[0] + 'page-' + str(i) + '.html'
+                soup = parse_url(next_page_url)
+                page_number_text = soup.find('li', {'class': 'current'}).text.strip()
+                current_page_number = page_number_text .split(' ')[1]
+
+                if int(current_page_number) < int(number_of_page):
+                    extract_product_info(next_page_url)
+                else:
+                    extract_product_info(next_page_url)
+                    export_data(data)
+
+
+start(url_site)
